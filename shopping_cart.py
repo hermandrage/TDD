ad5ecf9cf8b5e21233a8476ba720460294dc0e60
HermanDragesund <herman@gture.com>
2025-05-22 13:26:41 +0200

    Start gture Uni

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,23 +2,17 @@ from typing import List
 
 
 class ShoppingCart:
-    def __init__(self, max_size: int) -> None:
-        self.items: List[str] = []
-        self.max_size = max_size
+    def __init__(self) -> None:
+        pass
 
     def add(self, item: str):
-        if self.size() == self.max_size:
-            raise OverflowError("cannot add more items")
-        self.items.append(item)
+        pass
 
     def size(self) -> int:
-        return len(self.items)
+        return 0
 
     def get_items(self) -> List[str]:
-        return self.items
+        pass
 
     def get_total_price(self, price_map):
-        total_price = 0
-        for item in self.items:
-            total_price += price_map.get(item)
-        return total_price
+        pass
