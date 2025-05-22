931f48f9a71e4e25eacf7d7537fa6bd1cef0b044
HermanDragesund <herman@gture.com>
2025-05-22 14:38:22 +0200

    done with mocking

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -24,5 +24,11 @@ def test_when_add_more_than_max_items_should_fail(cart):
 def test_can_get_total_price(cart):
     cart.add("apple")
     cart.add("orange")
-    price_map = {"apple": 1.0, "orange": 2.0}
-    assert cart.get_total_price(price_map) == 3.0
+    item_database = ItemDatabase()
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        if item == "orange":
+            return 2.0
+    item_database.get = Mock(side_effect=mock_get_item)
+    assert cart.get_total_price(item_database) == 3.0
